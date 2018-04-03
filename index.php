dc00ee488f40648fe7c54d2ff37ebecef4aa41be
seregadgl20 <37291933+seregadgl20@users.noreply.github.com>
2018-04-03 13:01:09 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -10,10 +10,6 @@
 <script type="text/javascript" language="javascript"> 
 VK.Widgets.Comments('vk_comments', {limit: 10, width: '627', attach: '*'}); 
 </script> 
-
-</body> 
-</html>
-
 <?php
 
 $client_id = '6435178';
@@ -22,4 +18,10 @@ $url = 'http://hot-news.umc.by/';
 ?>
 
 <a href="https://oauth.vk.com/authorize?client_id=6435178&scope=<?php echo $scope;?>&redirect_uri=<?php echo $url;?>&display=DISPLAY&response_type=token">Push the button</a>
+
+</body> 
+</html>
+
+
+
 
